82fdff3cdc4d158c7c7815a2af79766c6006dae4
Falmesino <falmesino@gmail.com>
2026-08-02 10:28:46 +0700

    wip: empty pages

---
+++ includes/pages/users/add.php
@@ -0,0 +1,23 @@
+<div class="container d-flex flex-column">
+  <div class="row">
+    <div class="col-12">
+      <h3>Add User</h3>
+    </div>
+  </div><!--/ .row -->
+  <div class="row">
+    <div class="col-12">
+      <a
+        href="index.php?page=users"
+        type="button"
+        class="btn btn-dark btn-sm"
+      >
+        <span>Back to List</span>
+      </a>
+    </div>
+  </div><!--/ .row -->
+  <div class="row">
+    <div class="col-12">
+      <p>Form goes here</p>
+    </div>
+  </div>
+</div>
---
+++ includes/pages/users/index.php
@@ -0,0 +1,23 @@
+<div class="container d-flex flex-column">
+  <div class="row">
+    <div class="col-12">
+      <h3>Users</h3>
+    </div>
+  </div><!--/ .row -->
+  <div class="row">
+    <div class="col-12">
+      <a
+        href="index.php?page=users/add"
+        type="button"
+        class="btn btn-dark btn-sm"
+      >
+        <span>Add New User</span>
+      </a>
+    </div>
+  </div><!--/ .row -->
+  <div class="row">
+    <div class="col-12">
+      <p>Table goes here</p>
+    </div>
+  </div>
+</div>
